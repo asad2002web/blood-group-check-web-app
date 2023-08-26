a7af98a51c13bd1a011c6b179379e2afb09fdade
asad2002web <asad2002web@gmail.com>
2023-08-26 15:47:05 +0600

    designe completed

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,35 +1,38 @@
 import React from 'react'
 import './Layout.css'
-import { Card } from 'react-bootstrap'
+import CardAns from '../CardAns'
+import { Button } from 'react-bootstrap'
 const Layout = () => {
     return (
         <div className='main-layout bg-dark'>
             <h2 className='text-center text-white mb-3'>Check Your Blood Group</h2>
-            <Card>
-                <Card.Body>
-                    <Card.Title>What is broken A antigen blood?</Card.Title>
-                    <Card.Text>
-                        <div className='d-flex gap-4'>
-                            <div>
-                                <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-                                <label className="form-check-label" for="flexCheckDefault">
-                                    Yes
-                                </label>
-                            </div>
-                            <div>
-                                <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-                                <label className="form-check-label" for="flexCheckDefault">
-                                    No
-                                </label>
-                            </div>
-                        </div>
-                    </Card.Text>
-                    <Card.Link href="#">Card Link</Card.Link>
-                    <Card.Link href="#">Another Link</Card.Link>
-                </Card.Body>
-            </Card>
+            {
+                antigen.map((item, i) => <CardAns key={i} antigen={item} />)
+            }
+            <div className='mt-3 d-flex gap-5'>
+            <Button variant="danger">Check Blood</Button>
+      <Button variant="info">Reset</Button>
+            </div>
+            <div>
+                <h1 className='text-warning fs-3'>Your Blood Group Result : {"A+"}</h1>
+            </div>
         </div>
     )
 }
 
 export default Layout
+
+const antigen = [
+    {
+        "title": "What is broken A antigen blood?",
+        "response": "antigen-a"
+    },
+    {
+        "title": "What is broken B antigen blood?",
+        "response": "antigen-b"
+    },
+    {
+        "title": "What is broken Rh Factor blood?",
+        "response": "antigen-rh"
+    }
+]
